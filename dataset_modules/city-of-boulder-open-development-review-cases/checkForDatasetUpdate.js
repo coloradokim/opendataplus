785284126c6644265bb5d01d09cd4809e6b24704
Christian Ellis <christian@agilenine.com>
2015-05-13 21:28:32 -0600

    updated comments for checkfor

--- dataset_modules/city-of-boulder-open-development-review-cases/checkForDatasetUpdate.js
+++ dataset_modules/city-of-boulder-open-development-review-cases/checkForDatasetUpdate.js
@@ -1,3 +1,18 @@
 /**
  * Created by cellis on 5/13/15.
  */
+
+
+/**
+
+ go to the url:
+
+ http://data.opencolorado.org/api/3/action/package_show?id=city-of-boulder-open-development-review-cases
+
+ and in the resulting json,
+
+ result.resources[ where type="geojson" ].revision_timestamp
+
+ if the revision_timestamp is greater than the datasetLastUpdatedTimestamp, then return true
+
+ **/
