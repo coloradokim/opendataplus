1dfe50db1a80ab4a7b396f94072ad736db69c78f
Ximsce Inconnu <ximsce@gmail.com>
2015-06-28 15:31:20 -0600

    updating to use much easier request library.  fixed unit tests and added test cases for need update and does not need update.

--- tests/developmentReviewCasesTest.js
+++ tests/developmentReviewCasesTest.js
@@ -3,12 +3,45 @@
  */
 
 var should = require('chai').should(),
+    request = require('request'),
+    sinon = require('sinon'),
     checkForUpdates = require('../dataset_modules/city-of-boulder-open-development-review-cases/checkForDatasetUpdate');
 
 describe('development review cases dataset', function(){
 
-    it('checks for updates', function(done) {
-      testCallback = function(isUpdated) {
+    it('detects if dataset has not been updated', function(done) {
+      var fakePackageShowResponse = { 
+          result: { 
+            resources: [
+            { format: "geojson", revision_timestamp: "2015-06-08" }
+            ]
+          }
+      };
+      sinon
+        .stub(request, 'get')
+        .yields(null, null, JSON.stringify(fakePackageShowResponse));
+      var testCallback = function(isUpdated) {
+        isUpdated.should.not.be.true;
+        request.get.restore();
+        done();
+      };
+      checkForUpdates(testCallback);
+    });
+
+    it('detects if dataset has been updated', function(done) {
+      var fakePackageShowResponse = { 
+          result: { 
+            resources: [
+            { format: "geojson", revision_timestamp: "2015-06-18" }
+            ]
+          }
+      };
+      sinon
+        .stub(request, 'get')
+        .yields(null, null, JSON.stringify(fakePackageShowResponse));
+      var testCallback = function(isUpdated) {
+        isUpdated.should.be.true;
+        request.get.restore();
         done();
       };
       checkForUpdates(testCallback);
